c3fa4e1aead3c39d56fbf940422cc869856f35fa
Lucas Almeida Santiago <lucasasantiago@outlook.com>
2018-05-26 18:33:50 -0300

    Update Negociacao.java
    inserindo um erro

--- src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
+++ src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
@@ -13,7 +13,7 @@ public final class Negociacao implements Serializable{
 
 	public Negociacao(double preco, int quantidade, Calendar data) {
 		
-		if (data == null) {
+		if (data == null2) {
 			throw new IllegalArgumentException("data nao pode ser nula");
 		}
 		
